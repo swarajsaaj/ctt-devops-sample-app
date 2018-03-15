67e7c318ee7a2d65ff6c6f5378893092f327114c
Swaraj Pal <swaraj.pal@nagarro.com>
2018-03-15 15:03:01 +0530

    first commit

---
+++ src/main/java/com/nagarro/ctt/CttDevopsSampleAppApplication.java
@@ -0,0 +1,12 @@
+package com.nagarro.ctt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CttDevopsSampleAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CttDevopsSampleAppApplication.class, args);
+	}
+}
